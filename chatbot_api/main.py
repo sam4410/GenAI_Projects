c0b5cde7dbac437e7ae374e163a8db3be16a15a9
Manoj Sharma <64981579+sam4410@users.noreply.github.com>
2024-12-26 20:48:25 +0530

    Update main.py

--- chatbot_api/main.py
+++ chatbot_api/main.py
@@ -10,6 +10,13 @@ app = FastAPI(
     description="Endpoints for a hospital system graph RAG chatbot",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @async_retry(max_retries=10, delay=1)
 async def invoke_agent_with_retry(query: str):
